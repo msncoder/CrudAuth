b29556cd5bd7b090e6facd236ed4f2af3d7cde69
msncoder <msncoder1999@gmail.com>
2024-02-21 13:33:04 +0500

    configure Settings

--- CrudAuth/settings.py
+++ CrudAuth/settings.py
@@ -142,8 +142,8 @@ MESSAGE_TAGS = {
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'Your Google Account'
-EMAIL_HOST_PASSWORD = 'Your Google app Password'
+EMAIL_HOST_USER = 'Your Gmail'
+EMAIL_HOST_PASSWORD = 'Your App Password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 pip install Python 3.11.5
+python manage.py makemigrations CMS 
+python manage.py migrate CMS
+Go To Setting of Crud Auth and set email and app password     
